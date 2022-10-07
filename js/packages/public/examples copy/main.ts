c6044d92c19b7682f2e2e50b2c81693ce0523c43
Agustinus Theodorus <agustinustheoo@gmail.com>
2022-10-07 11:00:38 +0700

    feat(js-sdk): add public api endpoints

---
+++ js/packages/public/examples copy/main.ts
@@ -0,0 +1,12 @@
+import { getServerTime } from "..";
+
+async function main () {
+    const res = await getServerTime(
+        `https://api.bitwyre.com`
+    );
+    return res.data;
+}
+
+main()
+    .then(console.log)
+    .catch(e => console.log(e.response.data));
